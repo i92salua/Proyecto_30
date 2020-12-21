1c9999f8877398a98b6f3f861cb3dfa527b724a7
i92moloj <75329997+i92moloj@users.noreply.github.com>
2020-12-22 00:21:17 +0100

    Update visitante.h

--- visitante.h
+++ visitante.h
@@ -10,10 +10,10 @@ using namespace std;
 class Visitante{
 private:
     int tlf_, codigoR_, codigoV_, edad_;
-    string nombre_, apellidos_, nombrecompleto_, DNI_, Fech_, REsp_, auto_;
+    string nombre_, apellidos_, nombrecompleto_, DNI_, Fech_, REsp_, autori_;
 public:
     Visitante(string dni, string nombre="\0", string apellidos="\0", int edad=0, string Fech="\0",
-        string REsp="\0", string auto="\0", int tlf=0, int codigoR=0, int codigoV=0);
+        string REsp="\0", string autori="\0", int tlf=0, int codigoR=0, int codigoV=0);
     
     inline void setNombre(string nombre){nombre_=nombre;}
     inline string getNombre() const {return nombre_;}
@@ -24,12 +24,16 @@ public:
     inline void setEdad(int edad){edad_=edad;}
     inline int getEdad() const {return edad_;}
     inline void setTlf(int tlf){tlf_=tlf;}
-    inline date setFech(string Fech){Fech_=Fech;}
+    inline string setFech(string Fech){Fech_=Fech;}
     inline void setResp(string REsp){REsp_=REsp;}
-    inline void setAuto(string auto){auto_=auto;}
-    inline void setRuta(string codigoR){codigoR_=codigoR;}
-    inline void setCodigo(string codigoV){codigoV_=codigoV;}
+    inline void setAuto(string autori){autori_=autori;}
+    inline void setRuta(int codigoR){codigoR_=codigoR;}
+    inline void setCodigo(int codigoV){codigoV_=codigoV;}
     inline string nombrecompleto() const {return apellidos_+","+nombre_;}
-    bool MayordeEdad(int *n);
-    bool CompruebaDNI(string *n);
-}
+    bool MayordeEdad(int n);
+    bool revisor_dni(string &dni);
+    void add_visitante();
+    void del_visitante(char * fdatosv);
+};
+
+#endif
